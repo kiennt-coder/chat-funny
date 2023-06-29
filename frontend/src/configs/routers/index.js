8ed8c8f4d86666b73635ad17b1bf3c235dbda05d
elsaga.kiennt <kiennt.coder@gmail.com>
2023-06-30 00:19:07 +0700

    update route

--- frontend/src/configs/routers/index.js
+++ frontend/src/configs/routers/index.js
@@ -1,72 +1,101 @@
-import { createBrowserRouter } from "react-router-dom";
-// import asyncComponent from "../../utils/funcAsync";
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import asyncComponent from "../../utils/funcAsync";
 import Error from "../../pages/error";
-import nProgress from "nprogress";
+// import nProgress from "nprogress";
 
 const routes = createBrowserRouter(
-    [
-        // {
-        //     path: "/",
-        //     errorElement: <Error />,
-        //     children: [
-        //         {
-        //             path: "/",
-        //             Component: asyncComponent(() => import("../../layouts")),
-        //             exact: true,
-        //             children: [
-        //                 {
-        //                     path: "/chats",
-        //                     Component: asyncComponent(() =>
-        //                         import("../../pages/chats")
-        //                     ),
-        //                     children: [
-        //                         {
-        //                             path: ":id",
-        //                             Component: asyncComponent(() => <></>),
-        //                         },
-        //                     ],
-        //                 },
-        //             ],
-        //         },
-        //         {
-        //             path: "/signin",
-        //             Component: asyncComponent(() =>
-        //                 import("../../pages/login")
-        //             ),
-        //         },
-        //         {
-        //             path: "/signup",
-        //             Component: asyncComponent(() =>
-        //                 import("../../pages/register")
-        //             ),
-        //         },
-        //     ],
-        // },
-        {
-            path: "/",
-            errorElement: <Error />,
-            async lazy() {
-                nProgress.start();
-                let { default: Layout } = await import("../../layouts");
-                nProgress.done();
-                return { element: <Layout /> };
-            },
-            children: [
-                {
-                    path: "chats",
-                    async lazy() {
-                        nProgress.start();
-                        let { default: Chat } = await import(
-                            "../../pages/chats"
-                        );
-                        nProgress.done();
-                        return { element: <Chat /> };
-                    },
-                },
-            ],
-        },
-    ],
-    { basename: "/" }
+    createRoutesFromElements(
+        <Route path="/" errorElement={<Error />}>
+            <Route
+                path=""
+                Component={asyncComponent(() => import("../../layouts"))}
+            >
+                <Route
+                    path="chats"
+                    Component={asyncComponent(() =>
+                        import("../../pages/chats")
+                    )}
+                />
+            </Route>
+
+            <Route
+                path="signin"
+                Component={asyncComponent(() => import("../../pages/login"))}
+            />
+
+            <Route
+                path="signup"
+                Component={asyncComponent(() => import("../../pages/register"))}
+            />
+        </Route>
+    )
 );
+
+// const routes = createBrowserRouter(
+//     [
+//         // {
+//         //     path: "/",
+//         //     errorElement: <Error />,
+//         //     children: [
+//         //         {
+//         //             path: "/",
+//         //             Component: asyncComponent(() => import("../../layouts")),
+//         //             exact: true,
+//         //             children: [
+//         //                 {
+//         //                     path: "/chats",
+//         //                     Component: asyncComponent(() =>
+//         //                         import("../../pages/chats")
+//         //                     ),
+//         //                     children: [
+//         //                         {
+//         //                             path: ":id",
+//         //                             Component: asyncComponent(() => <></>),
+//         //                         },
+//         //                     ],
+//         //                 },
+//         //             ],
+//         //         },
+//         //         {
+//         //             path: "/signin",
+//         //             Component: asyncComponent(() =>
+//         //                 import("../../pages/login")
+//         //             ),
+//         //         },
+//         //         {
+//         //             path: "/signup",
+//         //             Component: asyncComponent(() =>
+//         //                 import("../../pages/register")
+//         //             ),
+//         //         },
+//         //     ],
+//         // },
+//         {
+//             path: "/",
+//             index: true,
+//             errorElement: <Error />,
+//             element: (
+//                 <div>{asyncComponent(() => import("../../layouts"))}</div>
+//             ),
+//             children: [
+//                 {
+//                     path: "chats",
+//                     element: (
+//                         <div>
+//                             {asyncComponent(() =>
+//                                 import("../../pages/chats")
+//                             )}
+//                         </div>
+//                     ),
+//                 },
+//             ],
+//         },
+//     ],
+//     { basename: "/" }
+// );
 
 export default routes;
