51c2db6fdd12cf02592f66e89f4c200bc5d3feb0
elsaga.kiennt <kiennt.coder@gmail.com>
2023-06-30 01:04:49 +0700

    update router lazy

--- frontend/src/configs/routers/index.js
+++ frontend/src/configs/routers/index.js
@@ -5,6 +5,8 @@ import {
 } from "react-router-dom";
 import asyncComponent from "../../utils/funcAsync";
 import Error from "../../pages/error";
+import LoadingComponent from "../../components/LoadingComponent";
+import React, { Suspense } from "react";
 // import nProgress from "nprogress";
 
 // const routes = createBrowserRouter(
@@ -40,20 +42,53 @@ import Error from "../../pages/error";
 // const Login = asyncComponent(() => import("../../pages/login"));
 // const Register = asyncComponent(() => import("../../pages/register"));
 
-import LayoutMain from "../../layouts";
-import Chat from "../../pages/chats";
-import Login from "../../pages/login";
-import Register from "../../pages/register";
+// import LayoutMain from "../../layouts";
+// import Chat from "../../pages/chats";
+// import Login from "../../pages/login";
+// import Register from "../../pages/register";
+
+const LayoutMain = React.lazy(() => import("../../layouts"));
+const Chat = React.lazy(() => import("../../pages/chats"));
+const Login = React.lazy(() => import("../../pages/login"));
+const Register = React.lazy(() => import("../../pages/register"));
 
 const routes = () => (
     <Route path="/" errorElement={<Error />}>
-        <Route path="" element={<LayoutMain />}>
-            <Route path="chats" element={<Chat />} />
+        <Route
+            path=""
+            element={
+                <Suspense fallback={<LoadingComponent />}>
+                    <LayoutMain />
+                </Suspense>
+            }
+        >
+            <Route
+                path="chats"
+                element={
+                    <Suspense fallback={<LoadingComponent />}>
+                        <Chat />
+                    </Suspense>
+                }
+            />
         </Route>
 
-        <Route path="signin" element={<Login />} />
+        <Route
+            path="signin"
+            element={
+                <Suspense fallback={<LoadingComponent />}>
+                    <Login />
+                </Suspense>
+            }
+        />
 
-        <Route path="signup" element={<Register />} />
+        <Route
+            path="signup"
+            element={
+                <Suspense fallback={<LoadingComponent />}>
+                    <Register />
+                </Suspense>
+            }
+        />
     </Route>
 );
 
