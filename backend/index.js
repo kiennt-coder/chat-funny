779bb13bd13e82b469d6842b9246c550f3be6571
Ngô Trung Kiên <118063793+kiennt-coder@users.noreply.github.com>
2023-07-07 02:42:17 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -32,14 +32,8 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const connectSocket = require("./services/socketIO");
 const io = new Server(server, {
-    handlePreflightRequest: (req, res) => {
-        const headers = {
-            "Access-Control-Allow-Headers": "Content-Type, Authorization",
-            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-            "Access-Control-Allow-Credentials": true
-        };
-        res.writeHead(200, headers);
-        res.end();
+    cors: {
+        origin: allowlist,
     }
 });
 
