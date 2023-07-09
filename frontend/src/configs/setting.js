9e6752c90f0e87aea7f828aff27454248ad0fe38
Ngô Trung Kiên <118063793+kiennt-coder@users.noreply.github.com>
2023-07-10 01:22:52 +0700

    Update setting.js

--- frontend/src/configs/setting.js
+++ frontend/src/configs/setting.js
@@ -1,7 +1,7 @@
 export default Object.freeze({
     APP_NAME: "Funny Chat",
-    API_URL: "http://chat-funny-api-dev.vercel.app/v1/",
-    SOCKET_URL: "http://chat-funny-api-dev.vercel.app",
+    API_URL: "https://chat-funny-api-dev.vercel.app/v1/",
+    SOCKET_URL: "https://chat-funny-api-dev.vercel.app",
     AUTHOR: "Kiennt",
     SITE_KEY: "6LfBPtsmAAAAAD1xtkSHLgSGrihge1EJyYPPFLIf",
     SECRET_KEY: "6LfBPtsmAAAAAHC00q-C88756ZM1K2FRRg24J0Vm",
