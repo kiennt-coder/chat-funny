ef4f7a591d32d689379457593a1aaefe8b2153b9
Ngô Trung Kiên <118063793+kiennt-coder@users.noreply.github.com>
2023-07-11 02:33:56 +0700

    Update setting.js

--- frontend/src/configs/setting.js
+++ frontend/src/configs/setting.js
@@ -1,7 +1,7 @@
 export default Object.freeze({
     APP_NAME: "Funny Chat",
-    API_URL: "https://funny-chat-c77dcae2e434.herokuapp.com//v1/",
-    SOCKET_URL: "https://funny-chat-c77dcae2e434.herokuapp.com/",
+    API_URL: "https://funny-chat-c77dcae2e434.herokuapp.com/v1/",
+    SOCKET_URL: "https://funny-chat-c77dcae2e434.herokuapp.com",
     AUTHOR: "Kiennt",
     SITE_KEY: "6LfBPtsmAAAAAD1xtkSHLgSGrihge1EJyYPPFLIf",
     SECRET_KEY: "6LfBPtsmAAAAAHC00q-C88756ZM1K2FRRg24J0Vm",
